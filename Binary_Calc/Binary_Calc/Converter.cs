3e2e4753ebb9a581a286c6f5c448b1a08b85c47f
ThiagoZx <TheThiagoo0@gmail.com>
2015-06-04 23:32:36 -0300

    Update - Conversor binário
    Criação da classe de conversão
    Criação do método de conversão de binário para decimal

---
+++ Binary_Calc/Binary_Calc/Converter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Calc
+{
+    class Converter
+    {
+        public string Cvtr_binToDec(string number) {
+           
+            char[] dec_number = number.ToCharArray();
+            Array.Reverse(dec_number);
+
+            double result = 0;
+
+            for ( int i = 0 ; i < dec_number.Length ; i++ ) {
+                int number_int = Convert.ToInt32(dec_number[i] - '0');
+                result = result + number_int * (Math.Pow(2, i));
+            }
+
+            return result.ToString();
+        }
+    }
+}
--- Binary_Calc/Binary_Calc/Form1.Designer.cs
+++ Binary_Calc/Binary_Calc/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -51,11 +52,21 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Outro simpático texto";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -69,6 +80,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
